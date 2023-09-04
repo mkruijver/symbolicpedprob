679f7a72c93db826ec93a9cf43a42da325d56199
mkruijver <maarten.kruijver@esr.cri.nz>
2023-09-04 10:33:11 +0200

    Add Simplify(..) to expect_equal_symbolic to improve resolution of equivalent expressions

--- tests/testthat/helper-expect_equal_symbolic.R
+++ tests/testthat/helper-expect_equal_symbolic.R
@@ -2,9 +2,9 @@
 
 expect_equal_symbolic <- function(object, expected){
 
-  symbolic_difference <- Ryacas::yac_str(paste0("(", object,")",
+  symbolic_difference <- Ryacas::yac_str(paste0("Simplify((", object,")",
                                                 " - ",
-                                                "(",expected,")"))
+                                                "(",expected,"))"))
 
   testthat::expect_equal(symbolic_difference, "0")
 }
